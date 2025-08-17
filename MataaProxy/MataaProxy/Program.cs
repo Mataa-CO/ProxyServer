c8891eaf328825d88cfee9e7bada11213dd67d11
Taha Alfoghi <102795632+tahaalfoghi@users.noreply.github.com>
2025-08-17 13:55:31 +0200

    push

---
+++ MataaProxy/MataaProxy/Program.cs
@@ -0,0 +1,31 @@
+using MataaProxy.Controllers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient();
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
